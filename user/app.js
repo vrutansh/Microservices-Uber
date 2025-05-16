4662371199797169d5ad4e5bff9cf43d3010e1a4
Vrutansh Patel <vrutanshpatel01@gmail.com>
2025-05-16 17:49:31 +0530

    Register controller

--- user/app.js
+++ user/app.js
@@ -1,6 +1,11 @@
+const dotenv = require('dotenv')
+dotenv.config()
 const express = require('express')
 const app = express()
+const cookieParser = require('cookie-parser')
 
-
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser)
 app.use('/', )
 module.exports = app
--- user/controllers/user.controller.js
+++ user/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const userModel = require('../models/user.model');
 
 const bcrypt = require('bcrypt');
+const { error } = require('console');
 
 const jwt = require('jsonwebtoken');
 
@@ -22,6 +23,11 @@ module.exports.register  = async (req,res) => {
 
         const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET, {expiresIn: '1h'})
 
+        res.cookie('token', token);
+
+        res.send({message: 'User Registered Succesfully'})
+    } catch (error){
+        res.status(500).json({message:error.message})
     }
 
 }
