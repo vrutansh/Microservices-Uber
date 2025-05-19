ed345c03e30b6cf75694e49968ce46553e910083
Vrutansh Patel <vrutanshpatel01@gmail.com>
2025-05-19 22:30:29 +0530

    init

---
+++ gateway/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const expressProxy = require('express-http-proxy')
+
+const app = express()
+
+
+app.use('/user', expressProxy('http://localhost:3001'))
+app.use('/captain', expressProxy('http://localhost:3002'))
+app.use('/ride', expressProxy('http://localhost:3003'))
+
+
+app.listen(3000, () => {
+    console.log('Gateway server listening on port 3000')
+})
